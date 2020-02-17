41a0e9a76f4744c122fb1067c5087eeab06116cb
Mark Starkman <me@markstarkman.com>
2020-02-17 09:02:13 -0500

    Setup custom fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document"
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html className="font-sans">
+        <Head>
+          <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Noto+Serif:400,700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ["Lato", "Helvetica", "Arial", "sans-serif"],
+      serif: ["Noto Serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"]
+    }
+  }
+}
