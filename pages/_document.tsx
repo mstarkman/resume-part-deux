bb828063d481ef602ba59fc084ff95dbd2a194f0
Mark Starkman <me@markstarkman.com>
2020-02-19 16:27:17 -0500

    Add favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,23 @@ class MyDocument extends Document {
       <Html className="font-sans">
         <Head>
           <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Noto+Serif:400,700&display=swap" rel="stylesheet" />
+
+          {/* <!-- generics --> */}
+          <link rel="icon" href="/favicons/favicon-32.png" sizes="32x32" />
+          <link rel="icon" href="/favicons/favicon-57.png" sizes="57x57" />
+          <link rel="icon" href="/favicons/favicon-76.png" sizes="76x76" />
+          <link rel="icon" href="/favicons/favicon-96.png" sizes="96x96" />
+          <link rel="icon" href="/favicons/favicon-128.png" sizes="128x128" />
+          <link rel="icon" href="/favicons/favicon-192.png" sizes="192x192" />
+          <link rel="icon" href="/favicons/favicon-228.png" sizes="228x228" />
+
+          {/* <!-- Android --> */}
+          <link rel="shortcut icon" sizes="196x196" href="/favicons/favicon-196.png" />
+
+          {/* <!-- iOS --> */}
+          <link rel="apple-touch-icon" href="/favicons/favicon-120.png" sizes="120x120" />
+          <link rel="apple-touch-icon" href="path/to/favicon-152.png" sizes="152x152" />
+          <link rel="apple-touch-icon" href="path/to/favicon-180.png" sizes="180x180" />
         </Head>
         <body>
           <Main />
