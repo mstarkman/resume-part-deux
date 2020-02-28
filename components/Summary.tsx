5d844bc42f5223f94a0a271310b03cc8025863af
Mark Starkman <me@markstarkman.com>
2020-02-28 16:18:39 -0500

    Change the summary

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -1,13 +1,25 @@
-import summaries from "../data/summaries"
 import Section from "./Section"
+
+const P = ({ children }) => <p className="mb-3 p-2">{children}</p>
 
 const Summary = () => (
   <Section title="Summary">
-    {summaries.map((summary, i) => (
-      <p className="mb-3 p-2" key={i}>
-        {summary}
-      </p>
-    ))}
+    <P>
+      <strong>Senior Software Engineer, Leader, Team builder and Mentor</strong>
+    </P>
+    <P>
+      <ul className="list-disc ml-8">
+        <li>Ruby on Rails – 8+ years’ experience</li>
+        <li>React, jQuery and JavaScript – 6+ years’ experience</li>
+        <li>Node.js – 2+ years’ experience</li>
+        <li>20+ years of experience – Database Administration, Management, .Net (C#), PostgreSQL, Microsoft SQL Server</li>
+      </ul>
+    </P>
+    <P>I have always had a passion for writing clear, concise and maintainable software.</P>
+    <P>
+      For my next opportunity, I’ll continue to write software and I want to incorporate more mentoring and coaching of lesser
+      experienced developers. My preference is to continue working remotely and I am available for work in Philadelphia area.
+    </P>
   </Section>
 )
 
