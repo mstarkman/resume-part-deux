a46d71b8b81e7d9e43d1ea9abf3c26d7ce15f4e3
Mark Starkman <me@markstarkman.com>
2020-03-10 12:25:27 -0400

    Make sure the site looks correct in safari

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Index = () => (
     <MobileHeader />
     <div className="w-full max-w-screen-xl mx-auto px-4 md:pl-0 lg:px-6 text-gray-800">
       <div className="flex">
-        <div className="hidden md:block min-h-screen overflow-y-visible w-1/3 z-90 sticky">
+        <div className="hidden h-full h-auto inset-0 md:block overflow-y-visible w-1/3 z-90 sticky">
           <div className="overflow-y-auto min-h-screen scrolling-touch sticky top-0">
             <div className="overflow-y-auto h-screen">
               <SideNav />
