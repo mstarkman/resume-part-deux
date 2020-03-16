8f8480b740ca4154a6c7b2ad033d3e76ede93682
Mark Starkman <me@markstarkman.com>
2020-03-16 13:25:51 -0400

    Make sure that all navigation can be seen on iphone landscape #10

--- components/MobileHeader.tsx
+++ components/MobileHeader.tsx
@@ -10,28 +10,26 @@ const MobileHeader = () => {
   }
 
   return (
-    <>
-      <div className="md:hidden bg-purple-700 text-yellow-100 fixed w-full px-5 py-4">
-        <div className="flex justify-between">
-          <div className="font-serif text-2xl">Mark Starkman</div>
-          <div className="flex flex-col justify-center">
-            <button className="flex flex-col pr-2" onClick={toggleNavShown}>
-              <div className="border-2 border-yellow-100 rounded-full w-0 h-0 mb-1"></div>
-              <div className="border-2 border-yellow-100 rounded-full w-0 h-0 mb-1"></div>
-              <div className="border-2 border-yellow-100 rounded-full w-0 h-0"></div>
-            </button>
-          </div>
+    <div className="md:hidden bg-purple-700 text-yellow-100 fixed w-full px-5 py-4">
+      <div className="flex justify-between">
+        <div className="font-serif text-2xl">Mark Starkman</div>
+        <div className="flex flex-col justify-center">
+          <button className="flex flex-col pr-2" onClick={toggleNavShown}>
+            <div className="border-2 border-yellow-100 rounded-full w-0 h-0 mb-1"></div>
+            <div className="border-2 border-yellow-100 rounded-full w-0 h-0 mb-1"></div>
+            <div className="border-2 border-yellow-100 rounded-full w-0 h-0"></div>
+          </button>
         </div>
-        {navShown ? (
-          <div className="md:hidden bg-purple-700 mt-4 pt-2 border-t border-yellow-100">
-            {navLinks.map((link, i) => (
-              <SideNavLink key={i} {...link} />
-            ))}
-            <SideNavLink {...navLinkRepo} />
-          </div>
-        ) : null}
       </div>
-    </>
+      {navShown ? (
+        <div className="md:hidden bg-purple-700 mt-4 pt-2 border-t border-yellow-100">
+          {navLinks.map((link, i) => (
+            <SideNavLink key={i} {...link} />
+          ))}
+          <SideNavLink {...navLinkRepo} />
+        </div>
+      ) : null}
+    </div>
   )
 }
 
