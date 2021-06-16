446d2aa1daa40d2f00c8756d5222855c633f3d74
Mark Starkman <me@markstarkman.com>
2021-06-16 15:55:50 -0400

    Update summary

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -5,18 +5,23 @@ const P = ({ children }) => <p className="p-2">{children}</p>
 const Summary = () => (
   <Section title="Summary">
     <P>
-      <strong>Senior Software Engineer, Leader, Team builder and Mentor</strong>
+      I am a long-time technologist who has years of experience at all layers of the tech stack. I am most proficient with the Ruby
+      on Rails stack and I have utilized it across various industries. Most recently, I’ve been working as a Site Reliability
+      Engineer engaged with engineering teams to improve observability and improve developer processes.
     </P>
-    <ul className="list-disc ml-8 p-2">
-      <li>Ruby on Rails – 8+ years’ experience</li>
-      <li>React, jQuery and JavaScript – 6+ years’ experience</li>
-      <li>Node.js – 2+ years’ experience</li>
-      <li>20+ years of experience – Database Administration, Management, .Net (C#), PostgreSQL, Microsoft SQL Server</li>
-    </ul>
-    <P>I have always had a passion for writing clear, concise and maintainable software.</P>
     <P>
-      For my next opportunity, I’ll continue to write software and I want to incorporate more mentoring and coaching of lesser
-      experienced developers. My preference is to continue working remotely and I am available for work in Philadelphia area.
+      I am a builder! I love to create and build new and creative features, functionality, and tooling. Using regular feedback from
+      users, I improve the products to make it more useful and beneficial.
+    </P>
+    <P>
+      Whether as a team lead or an individual contributor, I am most comfortable working as part of a team that is all working
+      towards the same goals. Working as part of a team and having my breadth of knowledge provides me with the ability to coach and
+      mentor lesser experienced technologists to level up the entire team.
+    </P>
+    <P>
+      I am passionate about working remotely as it enabled me to do the work that I love while saving on my commute time. This has
+      allowed me the opportunity to grow as a technologist by continuously learning about new technologies and techniques from my
+      own home.
     </P>
   </Section>
 )
